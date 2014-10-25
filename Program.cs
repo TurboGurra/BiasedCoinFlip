20ab6733d802e7b0258f110b9da7e2b85db09503
TurboGurra <turbogurra@msn.com>
2014-10-25 16:23:44 +0200

    Lade till en beskrivning av programmet

--- Program.cs
+++ Program.cs
@@ -7,14 +7,20 @@ namespace biased_coin_flip
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
+            /*
+             * Detta program är fungerar som en vanlig slantslingning,
+             * förutom att sidan som har hamnat uppåt flest gånger har
+             * en mindre chans att hamna uppåt nästa gång än den andra sidan
+             * beroende på skillnaden mellan sidornas "poäng"
+             */
             int heads = 1, tails = 1;
             Random random = new Random();
 
             for (int i = 0; i < 1000; i++)
             {
-                //if (random.Next(2) == 0)
+                //if (random.Next(2) == 0) //Utan balansering
                 if (random.Next(1, heads + tails) < heads)
                     tails++;
                 else
